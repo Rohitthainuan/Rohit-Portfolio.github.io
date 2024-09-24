8e9d6621ccf39a52119b0deda424c5c2245f3bd0
Rohit Kumar <162036634+Rohitthainuan@users.noreply.github.com>
2024-09-24 19:09:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll",(function){
-	header.classList.toggle ("sticky",window.scrollY >120)
+window.addEventListener ("scroll",function(){
+	header.classList.toggle ("sticky",window.scrollY >120);
 });
 
 let menu = document.querySelector('#menu-icon');
